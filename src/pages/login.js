b8f3be0eb809d74bfbc8e0dd936ebae5b7a8cb75
Jeremy Rice <jdavidrice@gmail.com>
2021-05-16 15:21:14 -0400

    Setup tailwind and add dependencies

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,3 +1,26 @@
+import { useState, useContext, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import FirebaseContext from '../context/firebase';
+
 export default function Login() {
-  return <p>I am the login page</p>;
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
+
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, usePassword] = useState('');
+
+  const [error, setError] = useState('');
+  const isInvalid = password === '' || emailAddress === '';
+
+  const handleLogin = () => {};
+
+  useEffect(() => {
+    document.title = 'Login - Instagram';
+  }, []);
+
+  return (
+    <div className="container flex mx-auto max-w-screen-md items-center h-screen">
+      <p>I have no idea!</p>
+    </div>
+  );
 }
