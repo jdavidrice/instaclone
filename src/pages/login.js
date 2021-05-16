0afee9427cc541b5b4d518a1d9e041965f8447f2
Jeremy Rice <jdavidrice@gmail.com>
2021-05-16 15:54:42 -0400

    Add email address form field to login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -20,7 +20,25 @@ export default function Login() {
 
   return (
     <div className="container flex mx-auto max-w-screen-md items-center h-screen">
-      <p>I have no idea!</p>
+      <div className="flex w-3/5">
+        <img src="/images/iphone-with-profile.jpg" alt="iPhone with Instagram app" />
+      </div>
+      <div className="flex flex-col w=2/5">
+        <h1 className="flex justify-center w-full">
+          <img src="/images/logo.png" alt="Instagram" className="mt-2 w-6/12 mb-4" />
+        </h1>
+        {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+
+        <form onSubmit={handleLogin} method="POST">
+          <input
+            aria-label="Enter your email address"
+            type="text"
+            placeholder="Email address"
+            className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+            onChange={({ target }) => console.log(target.value)}
+          />
+        </form>
+      </div>
     </div>
   );
 }
