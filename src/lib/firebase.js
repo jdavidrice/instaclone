815122d5cebcaef3ee60fab5dd39cce98b01ed67
Jeremy Rice <jdavidrice@gmail.com>
2021-05-17 23:43:47 -0400

    Fix type in firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -7,7 +7,7 @@ const config = {
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID
 };
 
