629c9483cc95a80542cd86d190b116718a82e980
Jeremy Rice <jdavidrice@gmail.com>
2021-05-18 13:14:21 -0400

    Add state to password and email on login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -7,7 +7,7 @@ export default function Login() {
   const { firebase } = useContext(FirebaseContext);
 
   const [emailAddress, setEmailAddress] = useState('');
-  const [password, usePassword] = useState('');
+  const [password, setPassword] = useState('');
 
   const [error, setError] = useState('');
   const isInvalid = password === '' || emailAddress === '';
@@ -23,7 +23,7 @@ export default function Login() {
       <div className="flex w-3/5">
         <img src="/images/iphone-with-profile.jpg" alt="iPhone with Instagram app" />
       </div>
-      <div className="flex flex-col w=2/5">
+      <div className="flex flex-col w-2/5">
         <h1 className="flex justify-center w-full">
           <img src="/images/logo.png" alt="Instagram" className="mt-2 w-6/12 mb-4" />
         </h1>
@@ -35,7 +35,14 @@ export default function Login() {
             type="text"
             placeholder="Email address"
             className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
-            onChange={({ target }) => console.log(target.value)}
+            onChange={({ target }) => setEmailAddress(target.value)}
+          />
+          <input
+            aria-label="Enter your password"
+            type="password"
+            placeholder="Password"
+            className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+            onChange={({ target }) => setPassword(target.value)}
           />
         </form>
       </div>
