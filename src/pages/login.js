155f6575dbb26e97d11e91bd540d07ad01fb9ddf
Jeremy Rice <jdavidrice@gmail.com>
2021-05-18 15:35:12 -0400

    Finish login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -2,6 +2,7 @@
 import { useState, useContext, useEffect } from 'react';
 import { useHistory, Link } from 'react-router-dom';
 import FirebaseContext from '../context/firebase';
+import * as ROUTES from '../constants/routes';
 
 export default function Login() {
   const history = useHistory();
@@ -13,7 +14,18 @@ export default function Login() {
   const [error, setError] = useState('');
   const isInvalid = password === '' || emailAddress === '';
 
-  const handleLogin = () => {};
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      await firebase.auth().signInWithEmailAndPassword(emailAddress, password);
+      history.push(ROUTES.DASHBOARD);
+    } catch (error) {
+      setEmailAddress('');
+      setPassword('');
+      setError(error.message);
+    }
+  };
 
   useEffect(() => {
     document.title = 'Login - Instagram';
@@ -25,7 +37,7 @@ export default function Login() {
         <img src="/images/iphone-with-profile.jpg" alt="iPhone with Instagram app" />
       </div>
       <div className="flex flex-col w-2/5">
-        <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4">
+        <div className="flex flex-col items-center bg-white p-4 rounded border border-gray-primary mb-4">
           <h1 className="flex justify-center w-full">
             <img src="/images/logo.png" alt="Instagram" className="mt-2 w-6/12 mb-4" />
           </h1>
@@ -49,14 +61,14 @@ export default function Login() {
             <button
               disabled={isInvalid}
               type="submit"
-              className={`bg-blue-500 text-white w-full rounded h-8 font-bold 
+              className={`bg-blue-medium text-white w-full rounded h-8 font-bold 
             ${isInvalid && ' opacity-50'}`}
             >
               Log In
             </button>
           </form>
         </div>
-        <div className="flex justify-center items-center flex-col w-full bg-white p-4 border border-gray-primary">
+        <div className="flex justify-center items-center flex-col w-full bg-white p-4  rounded border border-gray-primary">
           <p className="text-sm">
             Don't have an account?{` `}
             <Link to="/signup" className="font-bold text-blue-medium">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,28 @@
 module.exports = {
   future: {
     removeDeprecatedGapUtilities: true
+  },
+  theme: {
+    fill: (theme) => ({
+      red: theme('colors.red.primary')
+    }),
+    colors: {
+      white: '#fff',
+      blue: {
+        medium: '#005c98'
+      },
+      black: {
+        light: '#262626',
+        faded: '#00000059'
+      },
+      gray: {
+        base: '#616161',
+        background: '#fafafa',
+        primary: '#dbdbdb'
+      },
+      red: {
+        primary: '#ed4956'
+      }
+    }
   }
 };
