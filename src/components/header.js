d38f19b06ea6f058228549d064b240af28d4d999
Jeremy Rice <jdavidrice@gmail.com>
2021-05-22 19:16:27 -0400

    Add header component

--- src/components/header.js
+++ src/components/header.js
@@ -11,7 +11,50 @@ export default function Header() {
   return (
     <header className="h-16 bg-white border-b border-gray-primary mb-8">
       <div className="container mx-auto max-w-screen h-full">
-        <p>Hello</p>
+        <div className="flex justify-between h-full">
+          <div className="text-gray-700 text-center flex items-center align-items cursor-pointer">
+            <h1 className="flex justify-center w-full">
+              <Link to={ROUTES.DASHBOARD} aria-label="Instagram logo">
+                <img src="/images/logo.png" alt="Instagram" className="mt-2 w-6 w-6/12" />
+              </Link>
+            </h1>
+          </div>
+          <div className="text-gray-700 text-center flex items-center align-items">
+            {user ? (
+              <>
+                <Link to={ROUTES.DASHBOARD} aria-label="Dashboard">
+                  <svg
+                    className="w-8 mr-6 text-black-light cursor-pointer"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+                    />
+                  </svg>
+                </Link>
+
+                <button
+                  type="button"
+                  title="Sign Out"
+                  onClick={() => firebase.auth().signOut()}
+                  onKeyDown={(event) => {
+                    if (event.key === 'Enter') {
+                      firebase.auth().signOut();
+                    }
+                  }}
+                />
+              </>
+            ) : (
+              <></>
+            )}
+          </div>
+        </div>
       </div>
     </header>
   );
