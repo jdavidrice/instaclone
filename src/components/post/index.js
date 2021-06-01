ba168e54214ffda9a40e4153e662c5a245d8d6dd
Jeremy Rice <jdavidrice@gmail.com>
2021-06-01 16:15:20 -0400

    Create header for post component

---
+++ src/components/post/index.js
@@ -0,0 +1,19 @@
+import { useRef } from 'react-loading-skeleton';
+import PropTypes from 'prop-types';
+import Header from './header';
+
+export default function Post({ content }) {
+  return <Header username={content.username} />;
+}
+Post.propTypes = {
+  content: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    imageSrc: PropTypes.string.isRequired,
+    caption: PropTypes.string.isRequired,
+    docId: PropTypes.string.isRequired,
+    userLikedPhoto: PropTypes.bool.isRequired,
+    likes: PropTypes.array.isRequired,
+    comments: PropTypes.array.isRequired,
+    dateCreated: PropTypes.number.isRequired
+  })
+};
