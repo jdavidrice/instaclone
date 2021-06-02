64eab84cacde021b9d82c08075f75e84542dc81a
Jeremy Rice <jdavidrice@gmail.com>
2021-06-01 21:04:28 -0400

    Continue UI for timeline

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -1,9 +1,27 @@
 import { useRef } from 'react-loading-skeleton';
 import PropTypes from 'prop-types';
 import Header from './header';
+import Image from './image';
+import Actions from './actions';
+import Footer from './footer';
 
 export default function Post({ content }) {
-  return <Header username={content.username} />;
+  const commentInput = useRef(null);
+  const handleFocus = () => commentInput.current.focus();
+
+  return (
+    <div className="rounded col-span-4 border bg-white border-gray-primary mb-12">
+      <Header username={content.username} />
+      <Image src={content.imageSrc} caption={content.caption} />
+      <Actions
+        docId={content.docId}
+        totalLikes={content.likes.length}
+        likedPhoto={content.userLikedPhoto}
+        handleFocus={handleFocus}
+      />
+      <Footer caption={content.caption} username={content.username} />
+    </div>
+  );
 }
 Post.propTypes = {
   content: PropTypes.shape({
