26c40faecdb075a603d43bb61f0e846a089b8c02
Jeremy Rice <jdavidrice@gmail.com>
2021-06-03 21:17:29 -0400

    Add image component to post

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -1,11 +1,13 @@
 import { useRef } from 'react-loading-skeleton';
 import PropTypes from 'prop-types';
 import Header from './header';
+import Image from './image';
 
 export default function Post({ content }) {
   return (
-    <div className="rounded col-span-4 border bg-white border-gray-primary mb-16">
+    <div className="rounded col-span-4 border bg-white border-gray-primary mb-12">
       <Header username={content.username} />
+      <Image src={content.imageSrc} caption={content.caption} />
     </div>
   );
 }
--- src/hooks/use-photos.js
+++ src/hooks/use-photos.js
@@ -13,8 +13,6 @@ export default function usePhotos() {
       const [{ following }] = await getUserByUserId(userId);
       let followedUserPhotos = [];
 
-      console.log('following', following);
-
       if (following.length > 0) {
         followedUserPhotos = await getPhotos(userId, following);
       }
