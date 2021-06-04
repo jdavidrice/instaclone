cfec23daa293021772dabe54c5dfba363281ab9c
Jeremy Rice <jdavidrice@gmail.com>
2021-06-03 22:05:35 -0400

    Change to get useRef from 'react' in index.js. Page renders.

--- src/components/post/actions.js
+++ src/components/post/actions.js
@@ -1,12 +1,12 @@
 import { useState, useContext } from 'react';
 import PropTypes from 'prop-types';
 import FirebaseContext from '../../context/firebase';
-// import UserContext from '../../context/user';
+import UserContext from '../../context/user';
 
 export default function Actions({ docId, totalLikes, likedPhoto, handleFocus }) {
   const {
-    user: { uid: userId = '' }
-  } = useContext(useContext);
+    user: { uid: userId }
+  } = useContext(UserContext);
   const [toggleLiked, setToggleLiked] = useState(likedPhoto);
   const [likes, setLikes] = useState(totalLikes);
   const { firebase, FieldValue } = useContext(FirebaseContext);
