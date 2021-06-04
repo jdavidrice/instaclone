e6317d694c6ff8170177caf7674ee45ce5a124a0
Jeremy Rice <jdavidrice@gmail.com>
2021-06-03 21:08:29 -0400

    Create post folder and header component

---
+++ src/components/post/index.js
@@ -0,0 +1,23 @@
+import { useRef } from 'react-loading-skeleton';
+import PropTypes from 'prop-types';
+import Header from './header';
+
+export default function Post({ content }) {
+  return (
+    <div className="rounded col-span-4 border bg-white border-gray-primary mb-16">
+      <Header username={content.username} />
+    </div>
+  );
+}
+Post.propTypes = {
+  content: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    imageSrc: PropTypes.string.isRequired,
+    caption: PropTypes.string.isRequired,
+    docId: PropTypes.string.isRequired,
+    userLikedPhoto: PropTypes.bool.isRequired,
+    likes: PropTypes.array.isRequired,
+    comments: PropTypes.array.isRequired,
+    dateCreated: PropTypes.number.isRequired
+  })
+};
--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-nested-ternary */
 import Skeleton from 'react-loading-skeleton';
 import usePhotos from '../hooks/use-photos';
+import Post from './post';
 
 export default function Timeline() {
   const { photos } = usePhotos();
@@ -12,7 +13,7 @@ export default function Timeline() {
           <Skeleton count={4} width={640} height={500} className="mb-5" />
         </>
       ) : photos?.length > 0 ? (
-        photos.map((content) => <p key={content.docId}>{content.imageSrc}</p>)
+        photos.map((content) => <Post key={content.docId} content={content} />)
       ) : (
         <p className="text-center text-2xl">Follow people to see photos</p>
       )}
