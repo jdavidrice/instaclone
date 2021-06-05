c3d43664d37896ec87c61daee9ac6ba6c7a3c873
Jeremy Rice <jdavidrice@gmail.com>
2021-06-04 21:15:37 -0400

    Create add-comment.js component

---
+++ src/components/post/add-comment.js
@@ -0,0 +1,27 @@
+import { useState, useContext } from 'react';
+import PropTypes from 'prop-types';
+import FirebaseContext from '../../context/firebase';
+import UserContext from '../../context/user';
+
+export default function AddComment({ docId, comments: allComments, posted, commentInput }) {
+  const [comment, setComment] = useState('');
+  const { firebase, FieldValue } = useContext(FirebaseContext);
+  const {
+    user: { displayName }
+  } = useContext(UserContext);
+
+  const handleSubmitComment = (event) => {
+    event.preventDefault();
+
+    return null;
+  };
+
+  return null;
+}
+
+AddComment.propTypes = {
+  docId: PropTypes.string.isRequired,
+  comments: PropTypes.array.isRequired,
+  setComments: PropTypes.func.isRequired,
+  commentInput: PropTypes.object.isRequired
+};
--- src/components/post/comments.js
+++ src/components/post/comments.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { formatDistance } from 'date-fns';
 import { Link } from 'react-router-dom';
+import AddComment from './add-comment';
 
 export default function Comments({ docId, comments: allComments, posted, commentInput }) {
   const [comments, setComments] = useState(allComments);
@@ -25,6 +26,12 @@ export default function Comments({ docId, comments: allComments, posted, comment
           {formatDistance(posted, new Date())} ago
         </p>
       </div>
+      <AddComment
+        docId={docId}
+        comments={comments}
+        setComments={setComments}
+        commentInput={commentInput}
+      />
     </>
   );
 }
