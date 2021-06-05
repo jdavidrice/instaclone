bd1f3c4b58247ab1cd19b44385a04b2b3e6f5ec8
Jeremy Rice <jdavidrice@gmail.com>
2021-06-04 21:35:11 -0400

    Add comment field and post comment button

--- src/components/post/add-comment.js
+++ src/components/post/add-comment.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import FirebaseContext from '../../context/firebase';
 import UserContext from '../../context/user';
 
-export default function AddComment({ docId, comments: allComments, posted, commentInput }) {
+export default function AddComment({ docId, comments, setComments, commentInput }) {
   const [comment, setComment] = useState('');
   const { firebase, FieldValue } = useContext(FirebaseContext);
   const {
@@ -16,7 +16,37 @@ export default function AddComment({ docId, comments: allComments, posted, comme
     return null;
   };
 
-  return null;
+  return (
+    <div className="border-t border-gray-primary">
+      <form
+        className="flex justify-between pl-0 pr-5"
+        method="POST"
+        onSubmit={(event) =>
+          comment.length >= 1 ? handleSubmitComment(event) : event.preventDefault()
+        }
+      >
+        <input
+          aria-label="Add a comment"
+          autoComplete="off"
+          className="text-sm text-gray-base w-full mr-3 py-5 px-4"
+          type="text"
+          name="add-comment"
+          placeholder="Add a comment..."
+          value={comment}
+          onChange={({ target }) => setComment(target.value)}
+          ref={commentInput}
+        />
+        <button
+          className={`text-sm font-bold text-blue-medium ${!comment && 'opacity-50'}`}
+          type="button"
+          disabled={comment.length < 1}
+          onClick={handleSubmitComment}
+        >
+          Post
+        </button>
+      </form>
+    </div>
+  );
 }
 
 AddComment.propTypes = {
