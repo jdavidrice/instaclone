2924c87d4247f4aa4245cad43aa0b7c5a55704cc
Jeremy Rice <jdavidrice@gmail.com>
2021-06-28 20:23:36 -0400

    Render dashboard.test.js tests

--- src/__tests__/pages/dashboard.test.js
+++ src/__tests__/pages/dashboard.test.js
@@ -6,9 +6,11 @@ import Dashboard from '../../pages/dashboard';
 import UserContext from '../../context/user';
 import FirebaseContext from '../../context/firebase';
 import LoggedInUserContext from '../../context/logged-in-user';
+import { getPhotos, getSuggestedProfiles } from '../../services/firebase';
+import useUser from '../../hooks/use-user';
 import userFixture from '../../fixtures/logged-in-user';
 import photosFixture from '../../fixtures/timeline-photos';
-import { getPhotos } from '../../services/firebase';
+import suggestedProfilesFixture from '../../fixtures/suggested-profiles';
 
 const mockHistoryPush = jest.fn();
 jest.mock('react-router-dom', () => ({
@@ -29,6 +31,42 @@ describe('<Dashboard />', () => {
   it('renders the dashboard with a user profile and follows a user from the suggested profile sidebar', async () => {
     await act(async () => {
       getPhotos.mockImplementation(() => photosFixture);
+      getSuggestedProfiles.mockImplementation(() => suggestedProfilesFixture);
+      useUser.mockImplementation(() => ({ user: userFixture }));
+
+      const firebase = {
+        firestore: jest.fn(() => ({
+          collection: jest.fn(() => ({
+            doc: jest.fn(() => ({
+              update: jest.fn(() => Promise.resolve('User added'))
+            }))
+          }))
+        }))
+      };
+
+      const { getByText } = render(
+        <Router>
+          <FirebaseContext.Provider value={firebase}>
+            <UserContext.Provider
+              value={{
+                user: {
+                  uid: 'R4VG4Q2TxpNJmz9oEkcnZqV8YPQ2',
+                  displayName: 'jeremy'
+                }
+              }}
+            >
+              <LoggedInUserContext.Provider value={{ user: userFixture }}>
+                <Dashboard
+                  user={{
+                    uid: 'R4VG4Q2TxpNJmz9oEkcnZqV8YPQ2',
+                    displayName: 'jeremy'
+                  }}
+                />
+              </LoggedInUserContext.Provider>
+            </UserContext.Provider>
+          </FirebaseContext.Provider>
+        </Router>
+      );
     });
   });
 });
---
+++ src/fixtures/suggested-profiles.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    userId: 'R4VG4Q2TxpNJmz9oEkcnZqV8YPQ2',
+    username: 'jeremy',
+    fullName: 'Jeremy Rice',
+    emailAddress: 'jdavidrice@gmail.com',
+    following: [],
+    followers: [],
+    dateCreated: 1621729386194,
+    docId: 'rk3JmdJMVM74TdmReXTS'
+  }
+];
