cd857f4354f2dbff0261116f049e1380d01f9df1
Jeremy Rice <jdavidrice@gmail.com>
2021-07-02 16:28:26 -0400

    Change login photo to new instaclone version

--- src/pages/login.js
+++ src/pages/login.js
@@ -33,7 +33,7 @@ export default function Login() {
   return (
     <div className="container flex mx-auto max-w-screen-md items-center h-screen">
       <div className="flex w-3/5">
-        <img src="/images/iphone-with-profile.jpg" alt="iPhone with Instaclone app" />
+        <img src="/images/iphone-with-profile.png" alt="iPhone with Instaclone app" />
       </div>
       <div className="flex flex-col w-2/5">
         <div className="flex flex-col items-center bg-white p-4 rounded border border-gray-primary mb-4">
--- src/pages/sign-up.js
+++ src/pages/sign-up.js
@@ -65,7 +65,7 @@ export default function SignUp() {
   return (
     <div className="container flex mx-auto max-w-screen-md items-center h-screen">
       <div className="flex w-3/5">
-        <img src="/images/iphone-with-profile.jpg" alt="iPhone with Instaclone app" />
+        <img src="/images/iphone-with-profile.png" alt="iPhone with Instaclone app" />
       </div>
       <div className="flex flex-col w-2/5">
         <div className="flex flex-col items-center bg-white p-4 rounded border border-gray-primary mb-4">
