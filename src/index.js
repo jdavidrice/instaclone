e59c8c219fdb79453c1e158e254142646e804276
Jeremy Rice <jdavidrice@gmail.com>
2021-09-12 00:28:15 -0400

    Add self as logrocket user in index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import LogRocket from 'logrocket';
 import App from './App';
 import FirebaseContext from './context/firebase';
 import { firebase, FieldValue } from './lib/firebase';
 import './styles/app.css';
+
+LogRocket.init('l6icgt/instaclone');
+
+LogRocket.identify('jdavidrice@gmail.com', {
+  name: 'Jeremy Rice',
+  email: 'jdavidrice@gmail.com'
+});
 
 ReactDOM.render(
   <FirebaseContext.Provider value={{ firebase, FieldValue }}>
