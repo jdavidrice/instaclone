766624ea5fe273524fdea4035922b8d2494281d9
Jeremy Rice <jdavidrice@gmail.com>
2021-09-13 21:42:09 -0400

    Replace email with uid

--- src/services/logrocket.js
+++ src/services/logrocket.js
@@ -1,8 +1,6 @@
 import LogRocket from 'logrocket';
 
-LogRocket.identify('jdavidrice@gmail.com', {
+LogRocket.identify('R4VG4Q2TxpNJmz9oEkcnZqV8YPQ2', {
   name: 'Jeremy Rice',
   email: 'jdavidrice@gmail.com'
 });
-
-export default LogRocket;
