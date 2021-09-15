251ee87ce81964ede89ae07df3747a7cfd489160
Jeremy Rice <jdavidrice@gmail.com>
2021-09-15 00:37:57 -0400

    Add logrocket-react

--- src/services/logrocket.js
+++ src/services/logrocket.js
@@ -1,6 +1,8 @@
 import LogRocket from 'logrocket';
+import setupLogRocketReact from 'logrocket-react';
 
 LogRocket.init('l6icgt/instaclone');
+setupLogRocketReact(LogRocket);
 
 LogRocket.identify('jdavidrice@gmail.com', {
   name: 'Jeremy Rice',
